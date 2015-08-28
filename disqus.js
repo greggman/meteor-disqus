f6ba9f51af25db927730b49f246258e394feb0e4
Gregg Tavares <github@greggman.com>
2015-08-28 11:52:37 +0100

    require url, id, and title to be set

--- disqus.js
+++ disqus.js
@@ -1,61 +1,63 @@
 
 if (Meteor.isClient) {
 
-	disqus_identifier = null;
-	disqus_title = null;
-	disqus_url = null;
+    disqus_identifier = null;
+    disqus_title = null;
+    disqus_url = null;
 
+    Template.disqus.helpers({
+        'is_configurated' : function () {
+            return !!disqus_shortname;
+        }
+    });
 
-	
+    Template.disqus.rendered = function () {
+        var templateData;
 
-	
-	Template.disqus.helpers({
-		'is_configurated' : function () {
-			return !!disqus_shortname;
-		}
-	});
+        templateData = this.data;
+        Session.set('disqus.settings', templateData);
 
-	Template.disqus.rendered = function () {
-		var templateData;
+        if (window.DISQUS) {
+          if (!templateData.url || !templateData.identifier || !templateData.title) {
+              return;
+          }
+            DISQUS.reset({
+                reload : true,
+                config : function() {
+                    this.page.identifier = templateData.identifier;
+                    this.page.url = templateData.url;
 
-		templateData = this.data;
-		Session.set('disqus.settings', templateData);
+                    if (templateData.title) this.page.title = templateData.title;
+                    if (templateData.category_id) this.page.category_id = parseInt(templateData.category_id, 10);
+                }
+            });
+        } else {
+            Session.set('disqus.firstinit', true);
+        }
 
-		if (window.DISQUS) {
-			DISQUS.reset({
-				reload : true,
-				config : function() {
-					this.page.identifier = templateData.identifier || location.pathname;
-					this.page.url = templateData.url || location.href;
+    }
 
-					if (templateData.title) this.page.title = templateData.title;
-					if (templateData.category_id) this.page.category_id = parseInt(templateData.category_id, 10);
-				}
-			});
-		} else {
-			Session.set('disqus.firstinit', true);
-		}
+    Deps.autorun(function(){
+        if (Session.get('disqus.firstinit') && !window.DISQUS && disqus_shortname) {
 
-	}
+            var settings;
 
-	Deps.autorun(function(){
-		if (Session.get('disqus.firstinit') && !window.DISQUS && disqus_shortname) {
-			Session.set('disqus.firstinit', false);
+            settings = Session.get('disqus.settings');
 
-			var settings;
+            if (!settings.identifier || !settings.url || !settings.title) {
+                return
+            }
 
-			settings = Session.get('disqus.settings');
-			
-			disqus_identifier = settings.identifier || location.pathname;
-			disqus_url = settings.url || location.href;
+            Session.set('disqus.firstinit', false);
+            disqus_identifier = settings.identifier;
+            disqus_url = settings.url;
+            disqus_title = settings.title;
 
-			if (settings.title) disqus_title = settings.title;
-
-			(function() {
-				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-			})();
-		}
-	});
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        }
+    });
 }
